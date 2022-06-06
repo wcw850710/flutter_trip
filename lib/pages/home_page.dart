b80af58c6c2ee2a6cddbf23dcffad0509c5cbe26
吳俊緯 <wcw850710@gmail.com>
2022-06-06 21:13:54 +0800

    feat(home): 新增 swiper

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,11 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://s.newtalk.tw/album/album/1/5f292c6b25a74.jpg',
+    'http://s.newtalk.tw/album/album/1/5f2ba1d3d0c83.jpg',
+    'http://s.newtalk.tw/album/album/1/5f2bd3ae5e8ee.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('首頁'),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 160,
+              child: Swiper(
+                itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int i) {
+                  return Image.network(
+                    _imageUrls[i],
+                    fit: BoxFit.cover,
+                  );
+                },
+                pagination: const SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
